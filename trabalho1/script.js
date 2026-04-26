4a53067bef123c26eec7452a8e5432d3e9abb8ab
Fernanda <fernanda.fernandasouza.comp@gmail.com>
2026-04-26 00:05:59 -0300

    atualização trabalho 1

--- trabalho1/script.js
+++ trabalho1/script.js
@@ -1,5 +1,25 @@
-document.querySelectorAll('.character-item').forEach(item => {
-  item.addEventListener('click', function() {
-    this.classList.toggle('active');
-  });
-});
+const container = document.querySelectorAll(".personagem-card")[0]
+console.log(container)
+const lis = container.querySelectorAll("li")
+
+lis.forEach((li) => {
+    li.addEventListener("click", (event) => {
+      const descricao = li.querySelectorAll(".descricao")[0];
+      console.log(descricao.classList);
+      
+      lis.forEach((el)=>{
+        if(li != el){
+          const aux = el.querySelectorAll(".descricao")[0];
+          aux.classList.remove("active-descricao");
+          console.log(el);
+        }
+        
+      })
+      
+      descricao.classList.toggle("active-descricao");
+    })
+  
+})
+
+
+// lis.classList.toggle("visible");
